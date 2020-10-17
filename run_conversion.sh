681dd38ac0444c3c70a1e65cab8211fce5b1ed87
ole00 <ole00@post.cz>
2020-10-17 13:50:26 +0100

    added Linux conversion script
    The script does the following:
    - checks the java compiler is installed
    - compiles the tool into ./bin directory
    - find KiCad modules (footprints) in /usr/share/kicad/modules/*
    - converts all modules and stores them in ./Converted directory

---
+++ run_conversion.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# your java compiler path
+#JP=~/opt/java-8-openjdk-amd64/bin/
+
+# check java compiler
+JVER=`${JP}javac -version`
+if [ "$JVER" == "" ]; then
+	echo "ERROR: No java compiler found."
+	echo "Modify JP variable in this script if you have javac installed."
+	exit 1
+fi
+
+# uncomment to recompile the tool even if it exista
+# rm bin/*
+
+# clear output directory
+rm -rf Converted
+mkdir -p Converted
+
+# compile the tool
+if [ ! -f bin/KicadModuleToGEDA.class ]; then
+    ${JP}javac *.java
+	# check compilation failed
+	if [ $? != 0 ]; then
+		exit 1
+	fi
+
+	# move the compiled binaries to the bin directory
+    mkdir -p bin
+    mv *.class bin
+fi
+
+# find all kicad modules
+echo "Searching for KiCad Modules..."
+
+#find modules sub-directories
+MODULE_DIRS=`find  /usr/share/kicad/modules/ -maxdepth 1 -mindepth 1 -type d`
+
+#process all kicad module subdirectories
+for KDIR in ${MODULE_DIRS}; do
+	SUBDIR=$(basename ${KDIR})
+	echo "*** Module dir : ${SUBDIR}"
+	MODULES=`find ${KDIR} -type f -name "*.kicad_mod"`
+	#create output subdirectory
+	OUTDIR=Converted/${SUBDIR}
+	mkdir -p ${OUTDIR} 
+	# convert each and every module file
+	for KMODULE in ${MODULES} ; do
+		NAME=$(basename "${KMODULE%.*}")
+		echo "Converting module: ${NAME}"
+		${JP}java -cp bin KicadModuleToGEDA -q -nosummary -k ${KMODULE} -d ${OUTDIR}/
+	done
+done
