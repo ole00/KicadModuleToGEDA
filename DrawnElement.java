b48f4c878b6786f719a86352bbdb7958ff6e0902
ole00 <ole00@post.cz>
2020-10-17 18:52:33 +0100

    formatting: applied java astyle
    Fixed mixed indentations of tabs and spaces.
    The following astyle was applied:
    astyle --style=java -H -U -p

--- DrawnElement.java
+++ DrawnElement.java
@@ -4,21 +4,21 @@
 
 //    see LICENSE-gpl-v2.txt for software license
 //    see README.txt
-//    
+//
 //    This program is free software; you can redistribute it and/or
 //    modify it under the terms of the GNU General Public License
 //    as published by the Free Software Foundation; either version 2
 //    of the License, or (at your option) any later version.
-//    
+//
 //    This program is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
-//    
+//
 //    You should have received a copy of the GNU General Public License
 //    along with this program; if not, write to the Free Software
 //    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-//    
+//
 //    KicadModuleToGEDA Copyright (C) 2015 Erich S. Heinzle a1039181@gmail.com
 
 
@@ -47,14 +47,14 @@
 /* Layer identification (layer number)
 #define FIRST_COPPER_LAYER    0
 #define COPPER_LAYER_N        0
-#define LAYER_N_2             1     /* Numero layer 2 
-#define LAYER_N_3             2     /* Numero layer 3 
-#define LAYER_N_4             3     /* Numero layer 4 
-#define LAYER_N_5             4     /* Numero layer 5 
-#define LAYER_N_6             5     /* Numero layer 6 
-#define LAYER_N_7             6     /* Numero layer 7 
-#define LAYER_N_8             7     /* Numero layer 8 
-#define LAYER_N_9             8     /* Numero layer 9 
+#define LAYER_N_2             1     /* Numero layer 2
+#define LAYER_N_3             2     /* Numero layer 3
+#define LAYER_N_4             3     /* Numero layer 4
+#define LAYER_N_5             4     /* Numero layer 5
+#define LAYER_N_6             5     /* Numero layer 6
+#define LAYER_N_7             6     /* Numero layer 7
+#define LAYER_N_8             7     /* Numero layer 8
+#define LAYER_N_9             8     /* Numero layer 9
 #define LAYER_N_10            9     /* Numero layer 10
 #define LAYER_N_11            10    /* Numero layer 11
 #define LAYER_N_12            11    /* Numero layer 12
@@ -83,211 +83,199 @@
 #define LAST_NO_COPPER_LAYER  28
 #define NB_LAYERS             (LAST_NO_COPPER_LAYER + 1)
 
-#define LAYER_COUNT           32 */ 
+#define LAYER_COUNT           32 */
 
 // perhaps as a KicadDrawingElementWithPad class or similar??
 
-public class DrawnElement extends FootprintElementArchetype
-{
+public class DrawnElement extends FootprintElementArchetype {
 
-	String output = "";
+    String output = "";
 
-        long xCoordOneNm = 0;
-        long yCoordOneNm = 0;
-	long gEDAxCoordOne = 0;
-	long gEDAyCoordOne = 0;
-        long xCoordTwoNm = 0;
-        long yCoordTwoNm = 0;
-	long gEDAxCoordTwo = 0;
-	long gEDAyCoordTwo = 0;
+    long xCoordOneNm = 0;
+    long yCoordOneNm = 0;
+    long gEDAxCoordOne = 0;
+    long gEDAyCoordOne = 0;
+    long xCoordTwoNm = 0;
+    long yCoordTwoNm = 0;
+    long gEDAxCoordTwo = 0;
+    long gEDAyCoordTwo = 0;
 
-	long lineThicknessNm = 254000; // this is 10 mil in nanometres
-                        // which is 254 microns, which is 0.254 mm
-                        // which is 0.01 inches, which is 10 mil = 10 thou
-        long gEDAlineThickness = 100; // this is 10 mil in 0.1 mil units
+    long lineThicknessNm = 254000; // this is 10 mil in nanometres
+    // which is 254 microns, which is 0.254 mm
+    // which is 0.01 inches, which is 10 mil = 10 thou
+    long gEDAlineThickness = 100; // this is 10 mil in 0.1 mil units
 
-	int kicadLayer = 21; // 21 is set as the default, and is the top silkscreen layer
+    int kicadLayer = 21; // 21 is set as the default, and is the top silkscreen layer
 
-	String kicadDrawnSegmentDescriptor = "";
+    String kicadDrawnSegmentDescriptor = "";
 
-	public void DrawnElement()
-	{
-		output = "#Hmm, the no arg KicadDrawingElement constructor didn't do much";
-	}
+    public void DrawnElement() {
+        output = "#Hmm, the no arg KicadDrawingElement constructor didn't do much";
+    }
 
-	public String toString()
-	{
-		return kicadDrawnSegmentDescriptor;
-	}
+    public String toString() {
+        return kicadDrawnSegmentDescriptor;
+    }
 
-	public void populateElement(String arg, boolean metric)
-	{
-		kicadDrawnSegmentDescriptor = arg;
+    public void populateElement(String arg, boolean metric) {
+        kicadDrawnSegmentDescriptor = arg;
 
-		float parsedValue = 0;
-		
-		String[] tokens = arg.split(" ");
+        float parsedValue = 0;
+
+        String[] tokens = arg.split(" ");
 
 //		System.out.print("#The passed string:" + arg + "\n");
 
-		if (tokens[0].startsWith("DS"))
-		{
-			parsedValue = Float.parseFloat(tokens[1]);
-			xCoordOneNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[2]);
-                        yCoordOneNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[3]);
-                        xCoordTwoNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[4]);
-                        yCoordTwoNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[5]);
-                        lineThicknessNm = convertToNanometres(parsedValue, metric);
-			kicadLayer = Integer.parseInt(tokens[6]);
+        if (tokens[0].startsWith("DS")) {
+            parsedValue = Float.parseFloat(tokens[1]);
+            xCoordOneNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[2]);
+            yCoordOneNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[3]);
+            xCoordTwoNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[4]);
+            yCoordTwoNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[5]);
+            lineThicknessNm = convertToNanometres(parsedValue, metric);
+            kicadLayer = Integer.parseInt(tokens[6]);
 //			System.out.println("Kicad DS Layer is :" + kicadLayer);
-		}
-		else if (tokens[0].startsWith("fp_line"))
-                {
-			metric = true;
-                        parsedValue = Float.parseFloat(tokens[2]);
-                        xCoordOneNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[3]);
-                        yCoordOneNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[5]);
-                        xCoordTwoNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[6]);
-                        yCoordTwoNm = convertToNanometres(parsedValue, metric);
-                        parsedValue = Float.parseFloat(tokens[10]);
-                        lineThicknessNm = convertToNanometres(parsedValue, metric);
+        } else if (tokens[0].startsWith("fp_line")) {
+            metric = true;
+            parsedValue = Float.parseFloat(tokens[2]);
+            xCoordOneNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[3]);
+            yCoordOneNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[5]);
+            xCoordTwoNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[6]);
+            yCoordTwoNm = convertToNanometres(parsedValue, metric);
+            parsedValue = Float.parseFloat(tokens[10]);
+            lineThicknessNm = convertToNanometres(parsedValue, metric);
 // need to sort out layers though and parse text options though
-			if (tokens[8].startsWith("F.Cu")) {
-				kicadLayer = 15; // front most copper layer
-                        } else if (tokens[8].startsWith("B.Cu")) {
-                                kicadLayer = 0;
-                        } else if (tokens[8].startsWith("B.Paste")) {
-				kicadLayer = 18;
-                        } else if (tokens[8].startsWith("F.Paste")) {
-                                kicadLayer = 19;
-                        } else if (tokens[8].startsWith("B.Silk")) {
-                                kicadLayer = 20;
-                        } else if (tokens[8].startsWith("F.Silk")) {
-                                kicadLayer = 21;
-                        } else if (tokens[8].startsWith("B.Mask")) {
-                                kicadLayer = 22;
-                        } else if (tokens[8].startsWith("F.Mask")) {
-                                kicadLayer = 23;
-			}
+            if (tokens[8].startsWith("F.Cu")) {
+                kicadLayer = 15; // front most copper layer
+            } else if (tokens[8].startsWith("B.Cu")) {
+                kicadLayer = 0;
+            } else if (tokens[8].startsWith("B.Paste")) {
+                kicadLayer = 18;
+            } else if (tokens[8].startsWith("F.Paste")) {
+                kicadLayer = 19;
+            } else if (tokens[8].startsWith("B.Silk")) {
+                kicadLayer = 20;
+            } else if (tokens[8].startsWith("F.Silk")) {
+                kicadLayer = 21;
+            } else if (tokens[8].startsWith("B.Mask")) {
+                kicadLayer = 22;
+            } else if (tokens[8].startsWith("F.Mask")) {
+                kicadLayer = 23;
+            }
 //                        kicadLayer = Integer.parseInt(tokens[8]);
 //                      System.out.println("Kicad DS Layer is :" + kicadLayer);
-                }
+        }
 
-		else
-		{
-			System.out.println("Why wasn't the drawn segment passed something useful?");
-			output = "Hmm, a Draw Segment string was not passed to the object";
-		}
-	}
+        else {
+            System.out.println("Why wasn't the drawn segment passed something useful?");
+            output = "Hmm, a Draw Segment string was not passed to the object";
+        }
+    }
 
-        public String generateGEDAglyph(long xOffsetNm, long yOffsetNm, float magnificationRatio)
-        {
-	  // we take care of magnification here
-          // we can convert footprint silkscreen elements
-          // into a glyph with this
+    public String generateGEDAglyph(long xOffsetNm, long yOffsetNm, float magnificationRatio) {
+        // we take care of magnification here
+        // we can convert footprint silkscreen elements
+        // into a glyph with this
 
-               	gEDAxCoordOne = (long)((xCoordOneNm + xOffsetNm)*magnificationRatio/254);
-                                       // divide nm by 254 to produce
-               	gEDAyCoordOne = (long)((yCoordOneNm + yOffsetNm)*magnificationRatio/254);
-                                       // 0.01 mil units
-                gEDAxCoordTwo = (long)((xCoordTwoNm + xOffsetNm)*magnificationRatio/254);
-                gEDAyCoordTwo = (long)((yCoordTwoNm + yOffsetNm)*magnificationRatio/254);
+        gEDAxCoordOne = (long)((xCoordOneNm + xOffsetNm) * magnificationRatio / 254);
+        // divide nm by 254 to produce
+        gEDAyCoordOne = (long)((yCoordOneNm + yOffsetNm) * magnificationRatio / 254);
+        // 0.01 mil units
+        gEDAxCoordTwo = (long)((xCoordTwoNm + xOffsetNm) * magnificationRatio / 254);
+        gEDAyCoordTwo = (long)((yCoordTwoNm + yOffsetNm) * magnificationRatio / 254);
 
-		gEDAlineThickness = (long) ((magnificationRatio*lineThicknessNm)/ 254); // every 254 nm is 0.01 mil
+        gEDAlineThickness = (long)((magnificationRatio * lineThicknessNm) / 254); // every 254 nm is 0.01 mil
 
-		if (kicadLayer == 21) // i.e. drawing segment drawn on top silkscreen
-		{ // currently ignoring bottom silkscreen B.SilkS = 20, and
-		// B.Paste = 18, F.Paste = 19, since gEDA uses the clearance value
-		// and ignoring the F.Mask = 23 and B.Mask = 22 as well    
-			output = "SymbolLine[" +
-			gEDAxCoordOne + " " +  // we multiply by 10 for gEDAs .01mil units
-			gEDAyCoordOne + " " +
-			gEDAxCoordTwo + " " +
-			gEDAyCoordTwo + " " +
-			gEDAlineThickness + "]\n";
-		}
-		return output;
-	}
+        if (kicadLayer == 21) { // i.e. drawing segment drawn on top silkscreen
+            // currently ignoring bottom silkscreen B.SilkS = 20, and
+            // B.Paste = 18, F.Paste = 19, since gEDA uses the clearance value
+            // and ignoring the F.Mask = 23 and B.Mask = 22 as well
+            output = "SymbolLine[" +
+                     gEDAxCoordOne + " " +  // we multiply by 10 for gEDAs .01mil units
+                     gEDAyCoordOne + " " +
+                     gEDAxCoordTwo + " " +
+                     gEDAyCoordTwo + " " +
+                     gEDAlineThickness + "]\n";
+        }
+        return output;
+    }
 
-	public String generateGEDAelement(long xOffsetNm, long yOffsetNm, float magnificationRatio) // offsets in nm, and magnificationRatio as float
-	{
-		// we take care of magnification here
-        	// this allows a family of footprint silkscreens graphics to
-        	// be generated fairly easily, for example different sized 7
-		// or 17 segment LED displays
-		// Device outlines may need to be hand tweaked, as some
-		// device families share the same outline, despite different sized
-		// features across the family of device, i.e. 0.5 inch and
-		// 0.8 inch LED displays have the same outline silkscreen
-		// which doesn't need to be magnified
+    public String generateGEDAelement(long xOffsetNm, long yOffsetNm, float magnificationRatio) { // offsets in nm, and magnificationRatio as float
+        // we take care of magnification here
+        // this allows a family of footprint silkscreens graphics to
+        // be generated fairly easily, for example different sized 7
+        // or 17 segment LED displays
+        // Device outlines may need to be hand tweaked, as some
+        // device families share the same outline, despite different sized
+        // features across the family of device, i.e. 0.5 inch and
+        // 0.8 inch LED displays have the same outline silkscreen
+        // which doesn't need to be magnified
 
-               	gEDAxCoordOne = (long)((xCoordOneNm + xOffsetNm)*magnificationRatio/254);
-                                       // divide nm by 254 to produce
-               	gEDAyCoordOne = (long)((yCoordOneNm + yOffsetNm)*magnificationRatio/254);
-                                       // 0.01 mil units
-                gEDAxCoordTwo = (long)((xCoordTwoNm + xOffsetNm)*magnificationRatio/254);
-                gEDAyCoordTwo = (long)((yCoordTwoNm + yOffsetNm)*magnificationRatio/254);
+        gEDAxCoordOne = (long)((xCoordOneNm + xOffsetNm) * magnificationRatio / 254);
+        // divide nm by 254 to produce
+        gEDAyCoordOne = (long)((yCoordOneNm + yOffsetNm) * magnificationRatio / 254);
+        // 0.01 mil units
+        gEDAxCoordTwo = (long)((xCoordTwoNm + xOffsetNm) * magnificationRatio / 254);
+        gEDAyCoordTwo = (long)((yCoordTwoNm + yOffsetNm) * magnificationRatio / 254);
 
-	        long gEDAdefaultMetalClearance = 20;
-					// NB defined here in thousandths of an inch = mils
-                                        // (clearance/2) = minimum distance from pad/pin metal
-                                        // to nearest copper
-                                        // this gets multiplied by 100 for 0.01 mil units in output
+        long gEDAdefaultMetalClearance = 20;
+        // NB defined here in thousandths of an inch = mils
+        // (clearance/2) = minimum distance from pad/pin metal
+        // to nearest copper
+        // this gets multiplied by 100 for 0.01 mil units in output
 
-		String gEDAflag = "";
+        String gEDAflag = "";
 
-		if (kicadLayer == 0)
-		{
-			gEDAflag = "onsolder";
-		}
+        if (kicadLayer == 0) {
+            gEDAflag = "onsolder";
+        }
 
 //		gEDAxCoordOne = ((xCoordOneNm + xOffsetNm)/254); // divide nm by 254 to produce
 //		gEDAyCoordOne = ((yCoordOneNm + yOffsetNm)/254); // 0.01 mil units
 //              gEDAxCoordTwo = ((xCoordTwoNm + xOffsetNm)/254);
 //              gEDAyCoordTwo = ((yCoordTwoNm + yOffsetNm)/254);
 
-		gEDAlineThickness = (lineThicknessNm / 254); // every 254 nm is 0.01 mil
+        gEDAlineThickness = (lineThicknessNm / 254); // every 254 nm is 0.01 mil
 
-		if (kicadLayer == 21) // i.e. drawing segment drawn on top silkscreen
-		{ // currently ignoring bottom silkscreen B.SilkS = 20, and
-		// B.Paste = 18, F.Paste = 19, since gEDA uses the clearance value
-		// and ignoring the F.Mask = 23 and B.Mask = 22 as well    
-			output = "ElementLine[" +
-			gEDAxCoordOne + " " +  // we multiply by 10 for gEDAs .01mil units
-			gEDAyCoordOne + " " +
-			gEDAxCoordTwo + " " +
-			gEDAyCoordTwo + " " +
-			gEDAlineThickness + "]\n";
-		}
-		// this catches the drawing segment lines drawn on front or back copper
-		else if ((kicadLayer == 0) || (kicadLayer == 15))
-		{
-                        output = "Pad[" +
-                        gEDAxCoordOne + " " +
-                        gEDAyCoordOne + " " +
-                        gEDAxCoordTwo + " " +
-                        gEDAyCoordTwo + " " +
-                        gEDAlineThickness + " " +
-                        (100*gEDAdefaultMetalClearance) + " " +
-			"0 " + // let's give the pads zero solder mask relief
+        if (kicadLayer == 21) { // i.e. drawing segment drawn on top silkscreen
+            // currently ignoring bottom silkscreen B.SilkS = 20, and
+            // B.Paste = 18, F.Paste = 19, since gEDA uses the clearance value
+            // and ignoring the F.Mask = 23 and B.Mask = 22 as well
+            output = "ElementLine[" +
+                     gEDAxCoordOne + " " +  // we multiply by 10 for gEDAs .01mil units
+                     gEDAyCoordOne + " " +
+                     gEDAxCoordTwo + " " +
+                     gEDAyCoordTwo + " " +
+                     gEDAlineThickness + "]\n";
+        }
+        // this catches the drawing segment lines drawn on front or back copper
+        else if ((kicadLayer == 0) || (kicadLayer == 15)) {
+            output = "Pad[" +
+                     gEDAxCoordOne + " " +
+                     gEDAyCoordOne + " " +
+                     gEDAxCoordTwo + " " +
+                     gEDAyCoordTwo + " " +
+                     gEDAlineThickness + " " +
+                     (100 * gEDAdefaultMetalClearance) + " " +
+                     "0 " + // let's give the pads zero solder mask relief
 //                                (100*gEDAdefaultSolderMaskRelief + (kicadShapeYsizeNm/254)) + " " +
-			'"' + "DrawnElement" + "\" " +
+                     '"' + "DrawnElement" + "\" " +
 //                      '"' + kicadShapeNetName + "\" " +
-                        '"' + "DrawnElement" + "\" " +
+                     '"' + "DrawnElement" + "\" " +
 //                      '"' + kicadShapePadName + "\" " +
-                        '"' +
-                        gEDAflag +   // the flag is useful,  top vs onsolder placement
-                        '"' + "]\n";
-		}
-		return output;
-	}
+                     '"' +
+                     gEDAflag +   // the flag is useful,  top vs onsolder placement
+                     '"' + "]\n";
+        }
+        return output;
+    }
 
 
 	private long convertToNanometres(float rawValue, Boolean metricSystem)
--- FootprintElementArchetype.java
+++ FootprintElementArchetype.java
@@ -1,54 +1,44 @@
-public class FootprintElementArchetype
-{
+public class FootprintElementArchetype {
 
-	long xOffsetNm = 0;
-	long yOffsetNm = 0;
+    long xOffsetNm = 0;
+    long yOffsetNm = 0;
 
-	public long Xposition()
-	{
-		return xOffsetNm;
-	}
+    public long Xposition() {
+        return xOffsetNm;
+    }
 
-	public long Yposition()
-	{
-		return yOffsetNm;
-	}
+    public long Yposition() {
+        return yOffsetNm;
+    }
 
-	public void FootprintElementArchetype()
-	{
-		xOffsetNm = 0;
-		yOffsetNm = 0;
-	}
+    public void FootprintElementArchetype() {
+        xOffsetNm = 0;
+        yOffsetNm = 0;
+    }
 
-        public void FootprintElementArchetype(long x, long y)
-        {
-                xOffsetNm = x;
-                yOffsetNm = y;
-        }
+    public void FootprintElementArchetype(long x, long y) {
+        xOffsetNm = x;
+        yOffsetNm = y;
+    }
 
-	public String toString()
-	{
-		return("x: " + xOffsetNm + ", y: " + yOffsetNm);
-	}
+    public String toString() {
+        return ("x: " + xOffsetNm + ", y: " + yOffsetNm);
+    }
 
-	public String generateGEDAglyph(long xOffset, long yOffset, float magnificationRatio)
-	{
-		return "";
-	}
+    public String generateGEDAglyph(long xOffset, long yOffset, float magnificationRatio) {
+        return "";
+    }
 
-	public String generateGEDAelement(long xOffset, long yOffset, float magnificationRatio)
-	{
-		return "";
-	}
+    public String generateGEDAelement(long xOffset, long yOffset, float magnificationRatio) {
+        return "";
+    }
 
-	public void populateElement(String moduleDefinition, boolean metric)
-	{
-		System.out.println("You're not supposed to see this.");		
-	}
+    public void populateElement(String moduleDefinition, boolean metric) {
+        System.out.println("You're not supposed to see this.");
+    }
 
-        public void populateElement(String moduleDefinition, boolean metric, long minimumViaDrillSize)
-        {
-                System.out.println("You're not supposed to see this.");
-        }
-	
+    public void populateElement(String moduleDefinition, boolean metric, long minimumViaDrillSize) {
+        System.out.println("You're not supposed to see this.");
+    }
+
 }
